0870eaee64baa59ab0a9723a02cd9fa4b8555116
duncan19 <duncanrobbins19@gmail.com>
2022-01-20 10:42:14 -0800

    Add method practice

---
+++ MethodPractice/MethodPractice/Maths.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodPractice
+{
+    class Calc
+    {
+        public int Mult(int number)
+        {
+            number = (number * 2);
+            return number;
+        }
+        public int Div(int number)
+        {
+            number = (number / 3);
+            return number;
+        }
+        public int Sub(int number)
+        {
+            number = (number - 1);
+            return number;
+        }
+    }
+}
+
+
---
+++ MethodPractice/MethodPractice/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MethodPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please chose a number that you  wish to be divided by 3 multiplied by 2 and than subracted by 1 ");
+            //Maths userNum = new Maths();
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            Calc calc = new Calc();
+            Console.WriteLine(calc.Mult(userInput));
+            Console.WriteLine(calc.Div(userInput));
+            Console.WriteLine(calc.Sub(userInput));
+        }
+
+
+    }
+}
