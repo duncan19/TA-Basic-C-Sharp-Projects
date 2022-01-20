bcb03f0b2713d28ef0c97e63d0382f88d47bac40
duncan19 <duncanrobbins19@gmail.com>
2022-01-20 12:27:26 -0800

    Method Practice 3

---
+++ MethodPractice3/MethodPractice/MethodPractice/Maths.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodPractice
+{
+    class Calc
+    {
+        public int Mult(int number)
+        {
+            number = (number * 2);
+            return number;
+        }
+        public float Mult(float number)
+        {
+            number = (number * 3);
+            return number;
+        }
+        public int Mult(string number)
+        {
+
+           int firstNum = Convert.ToInt32(number);
+           return firstNum * 4;
+           
+        }
+        public int Div(int number)
+        {
+            number = (number / 3);
+            return number;
+        }
+        public int Sub(int number)
+        {
+            number = (number - 1);
+            return number;
+        }
+        public int twoNums(int number, int number2)
+        {
+           int finNumber = (number - number2);
+            return finNumber;
+        }
+    }
+}
+
+
---
+++ MethodPractice3/MethodPractice/MethodPractice/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MethodPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calc calc = new Calc();
+            //Console.WriteLine("Please chose a number that you  wish to multiply by 2");
+            ////Maths userNum = new Maths();
+            //int userInput = Convert.ToInt32(Console.ReadLine());
+            //float decimalValue = 0.0f;
+            //Console.WriteLine("Please chose a decimal number you wish to multiply by 3");
+            //decimalValue = Single.Parse(Console.ReadLine());
+            //Console.WriteLine("Please input a string number you wish to multiply by 2");
+            //string userString = (Console.ReadLine());
+      
+            Console.WriteLine("choose your 1st number to substract by the 2nd number");
+            int userNum1 = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                Console.WriteLine("choose your 2nd sunber to subract from the 1st one this step is optional");
+                int userNum2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(calc.twoNums(userNum1, userNum2));
+            }
+            catch (FormatException)
+            {
+                int userNum2 = 0;
+                Console.WriteLine(calc.twoNums(userNum1,userNum2));
+            }
+
+
+            
+            //Console.WriteLine(calc.Mult(userInput));
+            //Console.WriteLine(calc.Mult(decimalValue));
+            //Console.WriteLine(calc.Mult(userString));
+
+            
+
+
+        }
+
+
+    }
+}
