d46defb58107c12637efc94d242488a78c742fbc
duncan19 <duncanrobbins19@gmail.com>
2022-01-20 11:46:59 -0800

    Method Practice v2

---
+++ MethodPractice2/MethodPractice/MethodPractice/Maths.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodPractice
+{
+    class Calc
+    {
+        public int Mult(int number)
+        {
+            number = (number * 2);
+            return number;
+        }
+        public float Mult(float number)
+        {
+            number = (number * 3);
+            return number;
+        }
+        public int Mult(string number)
+        {
+
+           int firstNum = Convert.ToInt32(number);
+           return firstNum * 4;
+           
+        }
+        public int Div(int number)
+        {
+            number = (number / 3);
+            return number;
+        }
+        public int Sub(int number)
+        {
+            number = (number - 1);
+            return number;
+        }
+    }
+}
+
+
---
+++ MethodPractice2/MethodPractice/MethodPractice/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MethodPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please chose a number that you  wish to multiply by 2");
+            //Maths userNum = new Maths();
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            float decimalValue = 0.0f;
+            Console.WriteLine("Please chose a decimal number you wish to multiply by 3");
+            decimalValue = Single.Parse(Console.ReadLine());
+            Console.WriteLine("Please input a string number you wish to multiply by 2");
+            string userString = (Console.ReadLine());
+
+
+            Calc calc = new Calc();
+            Console.WriteLine(calc.Mult(userInput));
+            Console.WriteLine(calc.Mult(decimalValue));
+            Console.WriteLine(calc.Mult(userString));
+
+
+        }
+
+
+    }
+}
