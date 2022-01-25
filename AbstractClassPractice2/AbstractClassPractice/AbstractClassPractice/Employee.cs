4dee9266eb0c293f8c031989ccb0b0272376090d
duncan19 <duncanrobbins19@gmail.com>
2022-01-25 10:20:31 -0800

    abstract class practice

---
+++ AbstractClassPractice2/AbstractClassPractice/AbstractClassPractice/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractClassPractice
+{
+    class Employee : Person, IQuittable
+    {
+        public override void SayName()
+        {
+            Console.WriteLine("The Employee's name is :");
+            base.SayName();
+        }
+        public void Quit()
+        {
+            Console.WriteLine(this.firstName + " "+ this.lastName +" has Quit");
+        }
+    }
+}
---
+++ AbstractClassPractice2/AbstractClassPractice/AbstractClassPractice/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AbstractClassPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.firstName = "Sample";
+            employee.lastName = "Student";
+            employee.SayName();
+            employee.Quit();
+
+        }
+    }
+}
