606b47ae37a77f8842495299251d5b536a67bf93
duncan19 <duncanrobbins19@gmail.com>
2022-01-26 13:56:11 -0800

    operators practice

---
+++ OperatorsPractice/OperatorsPractice/Employee.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsPractice
+{
+
+    public class Employee
+    {
+        public int Id { get; set; }
+
+
+        public static bool operator== (Employee employee ,Employee employee2 )
+        {
+            if(employee.Id.Equals(employee2.Id))
+            {
+                Console.WriteLine("Overloaded ==");
+                return true;
+            }
+            else
+            {
+                return false;
+            }    
+           
+        }
+        public static bool operator!= (Employee employee, Employee employee2)
+        {
+            if (employee.Id != employee2.Id)
+            {
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("Overloaded !=");
+                return true;
+            }
+        }
+
+    }
+}
---
+++ OperatorsPractice/OperatorsPractice/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee();
+            Employee employee2 = new Employee();
+            employee1.Id = 2;
+            employee2.Id = 2;
+            Console.WriteLine(employee1.Id == employee2.Id);
+            Console.WriteLine(employee1.Id != employee2.Id);
+
+
+        }
+    }
+}
