f7dfa08dada1f5cb1ce5fab067db45315b8216e6
duncan19 <duncanrobbins19@gmail.com>
2022-02-03 11:20:21 -0800

    Add Console app of constructor practice

---
+++ ConstructorPractice/ConstructorPractice/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConstructorPractice
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Person(string name) : this(name, 10)
+        {
+        }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+            
+        }
+    }
+}
---
+++ ConstructorPractice/ConstructorPractice/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConstructorPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            const int age = 20;
+            Console.WriteLine("What is your name?");
+            var username = Console.ReadLine();
+            Person person = new Person(username);
+
+
+        }
+    }
+}
