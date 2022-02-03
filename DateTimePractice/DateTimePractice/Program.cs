962868a9610a6b9ab8653b05a04b1d809d005f29
duncan19 <duncanrobbins19@gmail.com>
2022-02-03 09:35:13 -0800

    Add date time console app

---
+++ DateTimePractice/DateTimePractice/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DateTimePractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime date = DateTime.Now;
+            Console.WriteLine(date);
+            Console.WriteLine("Please input a number");
+            int usernum = Convert.ToInt32(Console.ReadLine());
+            TimeSpan time = new TimeSpan(0, usernum, 0, 0);
+            DateTime combined = date.Add(time);
+            Console.WriteLine(combined);
+        }
+    }
+}
